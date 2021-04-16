31145235ed7e66682eaedce8e131fccf49741dad
Javier Perez <javier_perez95@icloud.com>
2021-04-16 08:11:15 -0500

    Initial Commit

---
+++ CoinFlip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoinFlip
+//
+//  Created by Javier Perez on 4/16/21.
+//  Copyright © 2021 Javier Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
